c523ecc361eaa26d5282350614b106f6f48d36e1
G1tHub-zer0 <quockhang0605@gmail.com>
2023-09-29 17:22:52 +0700

    bt5

--- Main/Main.java
+++ Main/Main.java
@@ -1,8 +1,23 @@
 package Main;
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập mã số sinh viên: ");
+        String maSoSinhVien = scanner.nextLine();
+
+        String regex = "^B[1-9]{7}$";
+
+        boolean isValid = Pattern.matches(regex, maSoSinhVien);
+
+        if (isValid) {
+            System.out.println("Mã số sinh viên hợp lệ.");
+        } else {
+            System.out.println("Mã số sinh viên không hợp lệ. Định dạng đúng là Bxxxxxxx (x là số từ 1-9).");
+        }
     }
 }
