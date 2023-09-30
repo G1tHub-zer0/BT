44b8149f1f15b7b437a70dda0f8e4cb1d0d6836a
G1tHub-zer0 <quockhang0605@gmail.com>
2023-09-30 19:58:05 +0700

    bt8

--- Main/Main.java
+++ Main/Main.java
@@ -5,10 +5,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int i = 1;
-        while(i <= 20){
-            System.out.println(n*i);
-            i++;
+        for(int i = 1; i <= n; i++){
+            snt(i);
+        }
+    }
+
+    static void snt(int n) {
+        int count = 0;
+        for(int i = 1; i <= n; i++){
+            if(n % i == 0){
+                count++;
+            }
+        }
+        if(count++ == 2){
+            System.out.println(n);
         }
     }
 }
