1d3f82274738287d3b33a42a06261da68e1debcc
G1tHub-zer0 <quockhang0605@gmail.com>
2023-09-30 20:38:25 +0700

    bt10

--- Main/Main.java
+++ Main/Main.java
@@ -1,21 +1,38 @@
 package Main;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        if(n <=0 || n > 20){
-            System.out.println("So "+ n + " khong hop le");
+        int[] a = new int[4];
+        LinkedList<String> list = new LinkedList<>();
+        for (int i = 0; i < 8; i++) {
+            list.add(sc.nextLine());
         }
-        int[] a = new int[20];
-        a[0] = 1;
-        a[1] = 1;
-        for(int i = 2; i < 20; i++){
-            a[i] = a[i - 2] + a[i - 1];
-            if(i == n){
-                System.out.println(a[i]);
+        if (list.size() != 8) {
+            System.out.println("Khong hop le");
+        }
+        int j = 0;
+        if (list.get(0) != "B") {
+            System.out.println("Khong hop le");
+        } else if (list.get(1) != "1") {
+            System.out.println("Khong hop le");
+        } else if (list.get(2) != "7") {
+            System.out.println("Khong hop le");
+        } else if (list.get(3) != "0") {
+            System.out.println("Khong hop le");
+        } else {
+            for (int i = 4; i < list.size(); i++) {
+                a[j] = Integer.valueOf(list.get(i));
+                j++;
             }
+            for (int i = 0; i < 4; i++) {
+                if (a[i] < 1 && a[i] > 9) {
+                    break;
+                }
+            }
+            System.out.println("Hop le");
         }
     }
 }
