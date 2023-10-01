5d563961d20126eea2efd4dcf8bfbe1ec1e6af65
G1tHub-zer0 <quockhang0605@gmail.com>
2023-10-01 20:35:11 +0700

    bt11

--- Main/Main.java
+++ Main/Main.java
@@ -1,38 +1,16 @@
 package Main;
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] a = new int[4];
-        LinkedList<String> list = new LinkedList<>();
-        for (int i = 0; i < 8; i++) {
-            list.add(sc.nextLine());
+        int n = sc.nextInt();
+        int ans = 0;
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++){
+            a[i] = sc.nextInt();
+            ans += a[i];
         }
-        if (list.size() != 8) {
-            System.out.println("Khong hop le");
-        }
-        int j = 0;
-        if (list.get(0) != "B") {
-            System.out.println("Khong hop le");
-        } else if (list.get(1) != "1") {
-            System.out.println("Khong hop le");
-        } else if (list.get(2) != "7") {
-            System.out.println("Khong hop le");
-        } else if (list.get(3) != "0") {
-            System.out.println("Khong hop le");
-        } else {
-            for (int i = 4; i < list.size(); i++) {
-                a[j] = Integer.valueOf(list.get(i));
-                j++;
-            }
-            for (int i = 0; i < 4; i++) {
-                if (a[i] < 1 && a[i] > 9) {
-                    break;
-                }
-            }
-            System.out.println("Hop le");
-        }
+        System.out.println(ans / a.length);
     }
 }
