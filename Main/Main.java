40be1846232eafee4af2a4de03e7bf4c06328882
G1tHub-zer0 <quockhang0605@gmail.com>
2023-10-02 19:05:09 +0700

    bt15

--- Main/Main.java
+++ Main/Main.java
@@ -1,31 +1,26 @@
 package Main;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
-    static void sort(int[] a){
-        int temp = 0;
-        for(int i = 0; i < a.length; i++){
-            for(int j = i + 1; j < a.length; j++){
-                if(a[i] > a[j]){
-                    temp = a[i];
-                    a[i] = a[j];
-                    a[j] = temp;
-                }
-            }
-        }
-    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int ans = 0;
-        int[] a = new int[n];
-        for(int i = 0; i < n; i++){
-            a[i] = sc.nextInt();
-            ans += a[i];
+        String[] a = new String[5];
+        for(int i = 0; i < 5; i++){
+            a[i] = sc.next();
+            if(a[i].length() != 8){
+                System.out.println("Sai roi");
+                break;
+            }
         }
-        sort(a);
-        for(int i = 0; i < n; i++){
-            System.out.println(a[i]);
+        String regex = "00[2,5]'L'[0,9]{4}";
+        for(int i = 0; i < 5; i++){
+            boolean isvalid  = Pattern.matches(regex, a[i]);
+            if(isvalid){
+                System.out.println("Sai roi");
+                break;
+            }
         }
+        System.out.println("Dung roi");
     }
 }
