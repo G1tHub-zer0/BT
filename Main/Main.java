83cf52ee0e876823d599b954a2aa67da9ee47b15
G1tHub-zer0 <quockhang0605@gmail.com>
2023-10-02 19:37:21 +0700

    bt18

--- Main/Main.java
+++ Main/Main.java
@@ -2,30 +2,15 @@ package Main;
 import java.util.Scanner;
 
 public class Main {
-    static boolean check(String s, char c){
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == c){
-                return true;
-            }
-        }
-        return false;
-    }
-    static void print(String s, char c){
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == c){
-                System.out.println(i);
-            }
-        }
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        char c = sc.next().charAt(0);
-        if(check(s, c) == true) {
-            print(s, c);
-        }else{
-            System.out.println("Khong ton tai trong chuoi");
+        for(int i = 0; i < s.length(); i++){
+            if(Character.isDigit(s.charAt(i))){
+                System.out.println("Co");
+                return;
+            }
         }
+        System.out.println("khong");
     }
 }
