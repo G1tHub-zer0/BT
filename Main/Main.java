cce030c9be763a9131fe42999963803f7b61a97c
G1tHub-zer0 <quockhang0605@gmail.com>
2023-10-02 19:27:55 +0700

    bt17

--- Main/Main.java
+++ Main/Main.java
@@ -10,14 +10,22 @@ public class Main {
         }
         return false;
     }
+    static void print(String s, char c){
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == c){
+                System.out.println(i);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         char c = sc.next().charAt(0);
         if(check(s, c) == true) {
-            System.out.println("Co");
+            print(s, c);
         }else{
-            System.out.println("Khong");
+            System.out.println("Khong ton tai trong chuoi");
         }
     }
 }
