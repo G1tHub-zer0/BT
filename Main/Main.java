a890063e846e919a0702511994c969644dc51eab
G1tHub-zer0 <quockhang0605@gmail.com>
2023-10-02 20:13:09 +0700

    bt20

--- Main/Main.java
+++ Main/Main.java
@@ -1,16 +1,26 @@
 package Main;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
+    static int check(String s){
+        int x = Integer.valueOf(s.substring(s.length()-1));
+        return x;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        int count = 0;
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == 'a'){
-                count++;
+        int c = s.length() - 1;
+        if (s.length() <= 20) {
+            if (!s.contains(" ")) {
+                if (s.charAt(0) >= 65 && s.charAt(0) <= 90) {
+                        if(check(s) >= 0 && check(s) <= 9){
+                            System.out.println("Duyet");
+                            return;
+                    }
+                }
             }
         }
-        System.out.println(count);
+        System.out.println("Khong duyet");
     }
 }
